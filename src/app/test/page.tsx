25c041d77534ccbaa94baace458e072d789627c8
choiminj <mjchoi@studiomate.kr>
2024-12-30 13:29:38 +0900

    라우터 테스트

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Test() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Link href="/test">go test</Link>
+      <Link href="/">go home</Link>
     </div>
   );
 }
