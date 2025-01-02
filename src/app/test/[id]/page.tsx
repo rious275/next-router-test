52bbfff2d6ea2692c693c1ccae2bb8ee4eaddf8e
choiminj <mjchoi@studiomate.kr>
2025-01-02 10:58:53 +0900

    asd

--- src/app/test/[id]/page.tsx
+++ src/app/test/[id]/page.tsx
@@ -1,22 +1,10 @@
-type PageProps = {
-  params: Promise<{ id: string }>;
-  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-};
+"use client";
 
-export async function generateStaticParams() {
-  const posts = await fetch(
-    "https://jsonplaceholder.typicode.com/posts",
-    {}
-  ).then(res => res.json());
+import { useParams } from "next/navigation";
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return posts.map((post: any) => ({
-    id: post.id.toString(),
-  }));
-}
+export default function Page() {
+  const params = useParams();
+  const id = params.id;
 
-export default async function Page({ params }: PageProps) {
-  const resolvedParams = await params;
-
-  return <div>{resolvedParams.id}포스트3</div>;
+  return <div>{id}포스트3</div>;
 }
